ba9e99a1b75f37fc1736b46c7630b48881c227f5
Miles <wangle2071@.com>
2025-05-20 17:10:56 +0800

    v202505201710

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,51 @@
+import { resolve } from 'path'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  modules: ['@nuxt/ui'],
-  css: ['~/assets/css/main.css'],
+  modules: [
+    '@nuxt/ui'
+  ],
+  css: [
+    './public/css/main.css'
+  ],
   ui: {
     fonts: false
+  },
+  ssr: true,
+  nitro: {
+    preset: 'static',
+    routeRules: {
+      '/**': { cors: true }
+    }
+  },
+  app: {
+    baseURL: '/nuxt',
+    head: {
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/nuxt/favicon.ico' },
+      ]
+    }
+  },
+  vite: {
+    server: {
+      fs: {
+        strict: false
+      }
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: undefined
+        }
+      }
+    },
+    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
+    // 配置资源基础路径
+    base: '/nuxt/'
   }
 })
